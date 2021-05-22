01b4c238e8ad6dc5ddb6b5e15a861aa9b9e14d9e
Marco Elizalde <contacto@marcoelizalde.com>
2021-05-22 08:47:01 -0500

    add(modules) se agrega los modulos iniciales

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from './config/config.module';
+import { ProductsModule } from './products/products.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, ConfigModule, ProductsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
