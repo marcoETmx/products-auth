e486973eb33376333c2c9346d4a8ba3c6ddc9a3b
Marco Elizalde <contacto@marcoelizalde.com>
2021-05-22 16:54:45 -0500

    add(config) agrega configuracion de variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,31 @@
 import { Module } from '@nestjs/common';
+import * as Joi from 'join';
+
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
-import { ConfigModule } from './config/config.module';
 import { ProductsModule } from './products/products.module';
 import { AuthModule } from './auth/auth.module';
+import { enviroments } from './enviroments';
+import config from './config';
 
 @Module({
-  imports: [DatabaseModule, ConfigModule, ProductsModule, AuthModule],
+  imports: [
+    DatabaseModule,
+    ProductsModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+      load: [config],
+      isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
