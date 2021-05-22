c85f43b2e3ce4d7a303101802545ed143b615c4e
Marco Elizalde <contacto@marcoelizalde.com>
2021-05-22 17:34:20 -0500

    add(db) agrega iniciacion de base de datos

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { Db } from 'mongodb';
 
+import config from './config';
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject('MONGO') private database: Db,
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
+  ) {}
   getHello(): string {
     return 'Hello World!';
   }
