1a01654c11d83d591eb8f10b89086717ff554066
Marco Elizalde <contacto@marcoelizalde.com>
2021-05-25 06:01:08 -0500

    add(auth) agrega soporte de roles

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors()
+
   const config = new DocumentBuilder()
     .setTitle('API')
     .setDescription('Products')
@@ -12,6 +14,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -9,11 +9,13 @@ import {
   Delete,
   HttpStatus,
   HttpCode,
-  UseGuards
+  UseGuards,
 } from '@nestjs/common';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../../auth/guards/jwt-auth-guard';
 import { Public } from '../../auth/decorators/public.decorator';
+import { Roles } from '../../auth/decorators/roles.decorador';
+import { Role } from '../../auth/models/roles.model';
 
 import { MongoIdPipe } from './../../common/mongo-id.pipe';
 import {
@@ -22,8 +24,9 @@ import {
   FilterProductsDto,
 } from '../dtos/products.dtos';
 import { ProductsService } from './../services/products.service';
+import { RolesGuard } from '../../auth/guards/roles.guard';
 
-@UseGuards(JwtAuthGuard)
+@UseGuards(JwtAuthGuard, RolesGuard)
 @ApiTags('products')
 @Controller('products')
 export class ProductsController {
@@ -43,16 +46,19 @@ export class ProductsController {
     return this.productsService.findOne(productId);
   }
 
+  @Roles(Role.ADMIN)
   @Post()
   create(@Body() payload: CreateProductDto) {
     return this.productsService.create(payload);
   }
 
+  @Roles(Role.ADMIN)
   @Put(':id')
   update(@Param('id') id: string, @Body() payload: UpdateProductDto) {
     return this.productsService.update(id, payload);
   }
 
+  @Roles(Role.ADMIN)
   @Delete(':id')
   delete(@Param('id') id: string) {
     return this.productsService.remove(id);
