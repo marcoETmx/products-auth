2b32c97cfeca082414b3eb4c2cd9e4d0bfd0be3a
Marco Elizalde <contacto@marcoelizalde.com>
2021-05-25 06:44:24 -0500

    add(auth) agrega jwt a endpoints de users

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -6,17 +6,25 @@ import {
   Body,
   Put,
   Delete,
+  UseGuards
 } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { UsersService } from '../services/users.service';
 import { CreateUserDto, UpdateUserDto } from '../dtos/user.dto';
+import { JwtAuthGuard } from '../../auth/guards/jwt-auth-guard';
+import { Public } from '../../auth/decorators/public.decorator';
+import { Roles } from '../../auth/decorators/roles.decorador';
+import { Role } from '../../auth/models/roles.model';
+import { RolesGuard } from '../../auth/guards/roles.guard';
 
+@UseGuards(JwtAuthGuard, RolesGuard)
 @ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  @Public()
   @Post()
   create(@Body() payload: CreateUserDto) {
     return this.usersService.create(payload);
@@ -27,6 +35,7 @@ export class UsersController {
     return this.usersService.update(id, payload);
   }
 
+  @Roles(Role.ADMIN)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(id);
